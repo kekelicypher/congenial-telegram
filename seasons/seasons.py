9685e09eb04e4eeb6273cc73f9c107935bbd77cd
Emmanuel Mishiwo <emmachmish@gmail.com>
2026-01-26 11:52:37 +0000

    almost done, left with error handling

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,15 +1,31 @@
 from datetime import date
+import sys
+import inflect
+
+p = inflect.engine()
 
 
 def main():
-    date_of_birth = input("Enter date of birth: ")
-    date.fromisocalendar(date_of_birth)
-    print(date_of_birth)
-    print(convert(date_of_birth))
+    try:
+        date_of_birth = input("Enter date of birth: ")    
+        print(convert(date_of_birth))
+    except Exception:
+        sys.exit("Invalid date")
 
 
-def convert(date):
-    ...
+def convert(date_of_birth):
+
+    date_of_birth = date.fromisoformat(date_of_birth)
+    age = date.today() - date_of_birth
+    age_in_minutes = age.days*24*60
+    return f'{p.number_to_words(age_in_minutes).replace("and ", "")} minutes'
+
+
+
+
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(convert(sys.argv[1]))
+        sys.exit()
     main()
